fc80b008125be35aa7ec6c954f956dfcf8611aaf
damatoaj <mr.arthurdamato@gmail.com>
2021-02-10 11:29:21 -0800

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Katie from './Katie.js'
+import Brian from './Brian.js'
+import Arthur from './Arthur.js'
 
 function App() {
   return (
     <div className="App">
     <h2>We are about!</h2>
+    <Arthur />
+    <Katie />
+    <Brian />
     </div>
   );
 }
